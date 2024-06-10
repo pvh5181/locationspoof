c7ca8b5eb6fe582a4eaae978e2f445ec46949ddf
pvh5181 <60400167+pvh5181@users.noreply.github.com>
2024-06-10 11:54:30 -0400

    Add files via upload

---
+++ locationspoof/Shared/locationspoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  locationspoofApp.swift
+//  Shared
+//
+//  Created by p on 4/24/21.
+//
+
+import SwiftUI
+
+@main
+struct locationspoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
